cbe14eed0fb114bbbdf96c1dd292787e6154e0b0
Yurii Rybachok <drummer2009@ukr.net>
2023-01-09 19:22:24 +0200

    add parameter to express router

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const axios = require('axios');
 require('dotenv').config();
 
 const PORT = process.env.PORT || 3000;
@@ -9,5 +10,17 @@ const app = express();
 
 // enable cors
 app.use(cors());
+
+// routes
+app.get('/api/search/:query', async (req, res) => {
+    try {
+        const query = req.params.query;
+        const results = await axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${process.env.API_KEY}`);
+        const data = results.data;
+        res.status(200).json(data);
+    } catch (err) {
+        res.status(500).json({error: err.message})
+    }
+})
 
 app.listen(PORT, () => console.log(`app has started on port: ${PORT}`));
