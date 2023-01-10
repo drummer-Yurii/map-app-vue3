d9e1f207a67067e440e8affd02b7502dfca149ce
Yurii Rybachok <drummer2009@ukr.net>
2023-01-10 11:09:29 +0200

    implement leaflet.js map

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,38 @@
 <template>
-  <div class="home">
-    <h1 class="text-xl text-red-500">Testing Tailwind</h1>
+  <div class="h-screen relative">
+    <div id="map" class="h-full z-[1]"></div>
   </div>
 </template>
 
 <script>
+import leaflet from 'leaflet';
+import { onMounted } from 'vue';
 
 export default {
   name: 'HomeView',
-  components: {
+  components: {},
+  setup() {
+    let map;
+    onMounted(() => {
+      // init map
+      map = leaflet.map('map').setView([28.538336, -81.379234], 10);
+
+      // add tile layer
+      leaflet
+        .tileLayer(
+          `https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${process.env.VUE_APP_API_KEY}`, 
+        {
+        attribution: 
+          'Map data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken: process.env.VUE_APP_API_KEY,
+      }
+      )
+      .addTo(map);
+    })
   }
 }
 </script>
